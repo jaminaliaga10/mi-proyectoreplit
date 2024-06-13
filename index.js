81bf8d28381a07ac5e9698d147eab4679719e799
jaminaliaga10 <156866578+jaminaliaga10@users.noreply.github.com>
2024-06-12 20:34:52 -0500

    Update index.js

--- index.js
+++ index.js
@@ -31,7 +31,7 @@ app.get('/', (req, res) => {
 });
 
 
-const PORT = process.env.PORT || 4000;
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
